998791c904f12b65238bd13b00f4b8268ba022e1
Mike Lawlor <shep34@msn.com>
2021-03-11 20:06:33 +0000

    complete basic sheep tests

--- test/Sheep.test.tsx
+++ test/Sheep.test.tsx
@@ -16,12 +16,65 @@ const defaultSheepProps: ISheep = {
   fieldId: 0,
 };
 
-describe("Sheep", () => {
+describe("Sheep render", () => {
   let wrapper: ShallowWrapper;
 
-  it("renders a sheep when passed the minimum required props", () => {
+  it("renders when passed the minimum required props", () => {
     wrapper = setup(defaultSheepProps);
     const sheep = findByTestAttr(wrapper, "sheep");
     expect(sheep).toHaveLength(1);
+  });
+
+  it("renders with a name and a gender displayed", () => {
+    wrapper = setup(defaultSheepProps);
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(sheep.find("div.sheep__name")).toHaveLength(1);
+    expect(sheep.find("div.sheep__name").text()).toBe('"Mutton"');
+    expect(sheep.find("div.sheep__gender")).toHaveLength(1);
+    expect(sheep.find("div.sheep__gender").text()).toBe("male");
+  });
+
+  it("assumes the correct class when branded", () => {
+    wrapper = setup({ ...defaultSheepProps, isBranded: true });
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(sheep.hasClass("sheep--branded")).toBe(true);
+  });
+
+  it("assumes the correct class when it is the primary selected sheep", () => {
+    wrapper = setup({ ...defaultSheepProps, sheepSelectedNumber: 1 });
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(sheep.hasClass("sheep--selectedSheep1")).toBe(true);
+  });
+
+  it("assumes the correct class when it is the secondary selected sheep", () => {
+    wrapper = setup({ ...defaultSheepProps, sheepSelectedNumber: 2 });
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(sheep.hasClass("sheep--selectedSheep2")).toBe(true);
+  });
+});
+
+describe("Sheep methods", () => {
+  let wrapper: ShallowWrapper;
+
+  it("calls selectSheep method when sheep is clicked", () => {
+    const mockSelectSheep = jest.fn();
+    wrapper = setup({ ...defaultSheepProps, selectSheep: mockSelectSheep });
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(mockSelectSheep).toBeCalledTimes(0);
+    sheep.simulate("click", {
+      stopPropagation: () => {},
+    });
+    expect(mockSelectSheep).toBeCalledTimes(1);
+  });
+
+  it("calls selectField method when sheep is clicked", () => {
+    const mockSelectField = jest.fn();
+    wrapper = setup({ ...defaultSheepProps, selectField: mockSelectField });
+    const sheep = findByTestAttr(wrapper, "sheep");
+    expect(mockSelectField).toBeCalledTimes(0);
+    sheep.simulate("click", {
+      stopPropagation: () => {},
+    });
+    expect(mockSelectField).toBeCalledTimes(1);
   });
 });
