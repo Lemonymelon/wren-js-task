1e3703682a7dc786cb726c29e842c1899aaffd91
Mike Lawlor <shep34@msn.com>
2021-03-12 13:22:03 +0000

    set up App testing suite

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow, ReactWrapper, ShallowWrapper } from "enzyme";
+import { shallow, mount, ReactWrapper, ShallowWrapper } from "enzyme";
 import { findByTestAttr } from "./utils";
 // import { IField, ISheep, IFarmer } from "../src/interfaces";
 // import { Field, Sheep, Farmer, App } from "../src/components";
@@ -10,19 +10,32 @@ const setupShallow = () => {
   return component;
 };
 
-// const setupMount = () => {
-//   const component = mount(<App />);
-//   return component;
-// };
+const setupMount = () => {
+  const component = mount(<App />);
+  return component;
+};
+
+let wrapper: ReactWrapper | ShallowWrapper;
 
 describe("App render", () => {
-  let wrapper: ReactWrapper | ShallowWrapper;
-
   it("renders an App upon initial load", () => {
     wrapper = setupShallow();
     const app = findByTestAttr(wrapper, "App");
     expect(app).toHaveLength(1);
   });
-});
 
-// App could have had an interface
+  it("renders an App with a farmer div upon initial load", () => {
+    wrapper = setupMount();
+    const app = findByTestAttr(wrapper, "App");
+    expect(app.find("div.farmer")).toHaveLength(1);
+  });
+
+  // mount field testing
+  // renders a field after farmer width, height controls are inputted, and button is clicked
+
+  // it("renders an App without fields or sheep upon initial load", () => {
+  //   wrapper = setupShallow();
+  //   const app = findByTestAttr(wrapper, "App");
+  //   expect(app.find("div.field")).toHaveLength(0);
+  // });
+});
